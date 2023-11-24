9e667d33f86f07a67606b0c01a0bd3119f7f16e4
李倩如 <liqianru@uino.com>
2023-11-24 17:42:34 +0800

    create project

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  devServer: {
+    hot: true,
+  },
+};
