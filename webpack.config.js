7890f0191d2cfc748452f92e8505e4fa49d5d381
李倩如 <liqianru@uino.com>
2023-11-27 17:48:04 +0800

    init: 搭建sass和ts环境

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /.ts$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
   devServer: {
     hot: true,
